06aa0b57355baf8bcd9b1b5b0ff2583e255e5fa0
Justin Melville <justinem2000@gmail.com>
2021-12-01 12:45:20 -0500

    File format

--- pytuxkart/actcri_agent.py
+++ pytuxkart/actcri_agent.py
@@ -38,44 +38,44 @@ class Actor(nn.Module):
     def __init__(self, in_dim: int, out_dim: int):
         """Initialize."""
         super(Actor, self).__init__()
-        
+
         self.hidden1 = nn.Linear(in_dim, 128)
-        self.mu_layer = nn.Linear(128, out_dim)     
-        self.log_std_layer = nn.Linear(128, out_dim)   
-        
+        self.mu_layer = nn.Linear(128, out_dim)
+        self.log_std_layer = nn.Linear(128, out_dim)
+
         initialize_uniformly(self.mu_layer)
         initialize_uniformly(self.log_std_layer)
 
     def forward(self, state: torch.Tensor) -> torch.Tensor:
         """Forward method implementation."""
         x = F.relu(self.hidden1(state))
-        
+
         mu = torch.tanh(self.mu_layer(x))
         log_std = F.softplus(self.log_std_layer(x))
         std = torch.exp(log_std)
-        
+
         dist = Normal(mu, std)
         action = dist.sample()
-        
+
         return action, dist
-    
+
+
 class Critic(nn.Module):
     def __init__(self, in_dim: int):
         """Initialize."""
         super(Critic, self).__init__()
-        
+
         self.hidden1 = nn.Linear(in_dim, 128)
         self.out = nn.Linear(128, 1)
-        
+
         initialize_uniformly(self.out)
 
     def forward(self, state: torch.Tensor) -> torch.Tensor:
         """Forward method implementation."""
         x = F.relu(self.hidden1(state))
         value = self.out(x)
-        
-        return value
 
+        return value
 
 
 class A2CAgent:
@@ -95,34 +95,36 @@ class A2CAgent:
         is_test (bool): flag to show the current mode (train / test)
     """
 
-    def __init__(self, pytux, track, gamma: float, entropy_weight: float, verbose=False, continue_training=False, screen_width=128, screen_height=96):
+    def __init__(self, pytux, track, gamma: float, entropy_weight: float, verbose=False, continue_training=False,
+                 screen_width=128, screen_height=96):
         """Initialize."""
         self.env = pytux
         self.track = track
         self.gamma = gamma
         self.entropy_weight = entropy_weight
-        
+
         # device: cpu / gpu
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu"
         )
         print(self.device)
-        
+
         # networks
         self.obs_dim = 4
         self.action_dim = 1
         self.actor = Actor(self.obs_dim, self.action_dim).to(self.device)
         if continue_training:
-            self.actor.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'actor.th'), map_location='cpu'))
+            self.actor.load_state_dict(
+                load(path.join(path.dirname(path.abspath(__file__)), 'actor.th'), map_location='cpu'))
         self.critic = Critic(self.obs_dim).to(self.device)
-        
+
         # optimizer
         self.actor_optimizer = optim.Adam(self.actor.parameters(), lr=1e-4)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=1e-3)
-        
+
         # transition (obs, log_prob, next_obs, reward, done)
         self.transition: list = list()
-        
+
         # total steps count
         self.total_step = 0
 
@@ -141,12 +143,12 @@ class A2CAgent:
         if isinstance(model, Actor):
             return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), 'actor.th'))
         raise ValueError("model type '%s' not supported!" % str(type(model)))
-    
+
     def load_model(self):
         r = Actor(self.obs_dim, self.action_dim).to(self.device)
         r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'actor.th'), map_location='cpu'))
         return r
-        
+
     def select_action(self, obs: np.ndarray, test_actor=None) -> np.ndarray:
         """Select an action from the input observation."""
         obs = torch.FloatTensor(obs).to(self.device)
@@ -159,9 +161,9 @@ class A2CAgent:
         if not self.is_test:
             log_prob = dist.log_prob(selected_action).sum(dim=-1)
             self.transition = [obs, log_prob]
-        
+
         return selected_action.clamp(-1.0, 1.0).cpu().detach().numpy()
-    
+
     def step(self, state, track, prev_loc, action, aim_point):
         restarted = False
         done = False
@@ -177,10 +179,10 @@ class A2CAgent:
             else:
                 off_track = True
                 self.off_track_tolerance = 0
-        
+
         end_track = np.isclose(kart.overall_distance / track.length, 1.0, atol=2e-3)
         if off_track:
-            if (self.total_step - self.restart_time > 30):
+            if self.total_step - self.restart_time > 30:
                 self.restart_time = self.total_step
                 if self.verbose:
                     print('off_track restarted ****************')
@@ -190,7 +192,7 @@ class A2CAgent:
                 print('end_track restarted ****************')
             restarted = True
             done = True
-        
+
         cur_loc = kart.distance_down_track
         # print('distance down track: ', cur_loc)
         speed_threshold = 0.5
@@ -198,14 +200,14 @@ class A2CAgent:
         loc_change = (cur_loc - prev_loc) if abs(cur_loc - prev_loc) < 2.5 else 0
         # print('off: ', abs_aim)
         # print('loc: ', cur_loc - prev_loc, 'cur_loc: ', cur_loc, 'prev_loc', prev_loc)
-        reward_off = - abs_aim * 30 # range (-25, 0)
-        reward_speed = 0 if (loc_change - speed_threshold) > 0 else (loc_change - speed_threshold) * 10 # range(-5, 0)
+        reward_off = - abs_aim * 30  # range (-25, 0)
+        reward_speed = 0 if (loc_change - speed_threshold) > 0 else (loc_change - speed_threshold) * 10  # range(-5, 0)
         reward = reward_speed + reward_off
-        
+
         if (self.total_step - self.restart_time < 5):
             reward = -30
-        return cur_loc, reward/2, restarted, done
-    
+        return cur_loc, reward / 2, restarted, done
+
     def update_kart(self, track, state):
         kart = state.players[0].kart
         cur_loc = kart.distance_down_track
@@ -213,14 +215,14 @@ class A2CAgent:
         proj = np.array(state.players[0].camera.projection).T
         view = np.array(state.players[0].camera.view).T
 
-        aim_point_world = self.env._point_on_track(cur_loc+TRACK_OFFSET, track)
+        aim_point_world = self.env._point_on_track(cur_loc + TRACK_OFFSET, track)
         aim_point_image = self.env._to_image(aim_point_world, proj, view)
-        current_vel = (np.linalg.norm(kart.velocity)-10)/10
+        current_vel = (np.linalg.norm(kart.velocity) - 10) / 10
 
         return aim_point_image, current_vel, aim_point_world, proj, view, kart
 
     def update_model(self) -> Tuple[torch.Tensor, torch.Tensor]:
-        """Update the model by gradient descent."""  
+        """Update the model by gradient descent."""
         obs, log_prob, next_obs, reward, done = self.transition
 
         # Q_t   = r + gamma * V(s_{t+1})  if obs != Terminal
@@ -237,7 +239,7 @@ class A2CAgent:
         pred_value = self.critic(obs)
         targ_value = reward + self.gamma * self.critic(next_obs) * mask
         value_loss = F.smooth_l1_loss(pred_value, targ_value.detach())
-        
+
         # update value
         self.critic_optimizer.zero_grad()
         value_loss.backward()
@@ -257,8 +259,8 @@ class A2CAgent:
         # print('value_loss: ', value_loss)
 
         return policy_loss.item(), value_loss.item()
-    
-    def init_track(self, track = 'lighthouse'):
+
+    def init_track(self, track='lighthouse'):
         if self.env.k is not None and self.env.k.config.track == track:
             # print('init restart +++++++++++++++++++++')
             self.env.k.restart()
@@ -274,7 +276,7 @@ class A2CAgent:
             self.env.k = pystk.Race(config)
             self.env.k.start()
             self.env.k.step()
-        
+
         return pystk.WorldState(), pystk.Track()
 
     def train(self, num_frames: int, plotting_interval: int = 2500):
@@ -286,7 +288,7 @@ class A2CAgent:
         prev_loc = 0
         best_score = -9999999999999
         last_rescue = 0
-        
+
         state, track = self.init_track()
         state.update()
         track.update()
@@ -294,7 +296,7 @@ class A2CAgent:
         if self.verbose:
             # show video
             fig, ax = plt.subplots(1, 1)
-        
+
         for self.total_step in range(1, num_frames + 1):
             aim_point, vel, _, _, _, kart = self.update_kart(track, state)
             obs = np.array((aim_point[0], aim_point[1], vel, kart.distance_down_track))
@@ -307,7 +309,7 @@ class A2CAgent:
             next_obs = np.array((next_aim_point[0], next_aim_point[1], next_vel, kart.distance_down_track))
 
             self.transition.extend([next_obs, reward, done])
-            
+
             actor_loss, critic_loss = self.update_model()
 
             actor_epoch_lossess.append(actor_loss)
@@ -319,11 +321,11 @@ class A2CAgent:
                 actor_epoch_lossess, critic_epoch_losses = [], []
 
             score += reward
-            
+
             if vel < 1.0 and self.total_step - last_rescue > RESCUE_TIMEOUT:
                 last_rescue = self.total_step
                 restarted = True
-            
+
             # if episode ends
             if restarted:
                 state, track = self.init_track()
@@ -342,14 +344,17 @@ class A2CAgent:
                 ax.set_title(title)
                 ax.imshow(self.env.k.render_data[0].image)
                 WH2 = np.array([self.env.config.screen_width, self.env.config.screen_height]) / 2
-                ax.add_artist(plt.Circle(WH2*(1+self.env._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
-                ax.add_artist(plt.Circle(WH2*(1+self.env._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
+                ax.add_artist(
+                    plt.Circle(WH2 * (1 + self.env._to_image(kart.location, proj, view)), 2, ec='b', fill=False,
+                               lw=1.5))
+                ax.add_artist(
+                    plt.Circle(WH2 * (1 + self.env._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False,
+                               lw=1.5))
                 plt.pause(1e-3)
                 print('observation: ', obs)
                 print('steering: ', steer)
                 print('time frame: ', self.total_step)
                 print('score: ', score, 'reward: ', reward)
-
 
             if self.total_step % plotting_interval == 0:
                 best_score = -9999999999999
@@ -360,7 +365,7 @@ class A2CAgent:
                     self._plot_cmd(self.total_step, best_score, actor_losses, critic_losses)
 
         self.env.close()
-    
+
     def test(self, max_frame):
         """Test the agent."""
         self.is_test = True
@@ -371,7 +376,7 @@ class A2CAgent:
         best_score = -9999999999999
         last_rescue = 0
         count = 0
-        
+
         state, track = self.init_track()
         state.update()
         track.update()
@@ -381,7 +386,7 @@ class A2CAgent:
         if self.verbose:
             # show video
             fig, ax = plt.subplots(1, 1)
-        
+
         for cur_frame in range(max_frame):
             if count >= 9:
                 break
@@ -401,7 +406,7 @@ class A2CAgent:
                 # print('rescue', 'cur frame: ', cur_frame, ' last rescue: ', last_rescue)
                 last_rescue = cur_frame
                 restarted = True
-            
+
             # if episode ends
             if restarted:
                 state, track = self.init_track()
@@ -416,43 +421,48 @@ class A2CAgent:
                 score = 0
 
             if self.verbose:
-                title = "Time frame: {}; Score: {:.2f}; Attempt: {}".format(cur_frame, score, count+1)
+                title = "Time frame: {}; Score: {:.2f}; Attempt: {}".format(cur_frame, score, count + 1)
                 ax.clear()
                 ax.set_title(title)
                 ax.imshow(self.env.k.render_data[0].image)
                 WH2 = np.array([self.env.config.screen_width, self.env.config.screen_height]) / 2
-                ax.add_artist(plt.Circle(WH2*(1+self.env._to_image(kart.location, proj, view)), 2, ec='b', fill=False, lw=1.5))
-                ax.add_artist(plt.Circle(WH2*(1+self.env._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False, lw=1.5))
+                ax.add_artist(
+                    plt.Circle(WH2 * (1 + self.env._to_image(kart.location, proj, view)), 2, ec='b', fill=False,
+                               lw=1.5))
+                ax.add_artist(
+                    plt.Circle(WH2 * (1 + self.env._to_image(aim_point_world, proj, view)), 2, ec='r', fill=False,
+                               lw=1.5))
                 plt.pause(1e-3)
                 print('observation: ', obs)
                 print('steering: ', steer)
                 print('time frame: ', cur_frame)
                 print('score: ', score, 'reward: ', reward)
-        
+
         # print("score: ", score)
         self.env.close()
 
     def _plot_cmd(
-        self, 
-        frame_idx: int, 
-        score: float,
-        actor_losses: List[float], 
-        critic_losses: List[float]
+            self,
+            frame_idx: int,
+            score: float,
+            actor_losses: List[float],
+            critic_losses: List[float]
     ):
         print('==========================')
         print('frame: ', frame_idx)
         print('scores: ', score)
         print('actor losses: ', actor_losses[-1])
         print('critic losses: ', critic_losses[-1])
-    
+
     def _plot(
-        self, 
-        frame_idx: int, 
-        score: float, 
-        actor_losses: List[float], 
-        critic_losses: List[float], 
+            self,
+            frame_idx: int,
+            score: float,
+            actor_losses: List[float],
+            critic_losses: List[float],
     ):
         """Plot the training progresses."""
+
         def subplot(loc: int, title: str, values: List[float]):
             plt.subplot(loc)
             plt.title(title)
@@ -470,6 +480,7 @@ class A2CAgent:
             subplot(loc, title, values)
         plt.show()
 
+
 def main(pytux, track, verbose=False, test=False, continue_training=False):
     num_frames = 1000000
     gamma = 0.9
@@ -480,6 +491,7 @@ def main(pytux, track, verbose=False, test=False, continue_training=False):
         agent.test(num_frames)
     else:
         agent.train(num_frames)
+
 
 if __name__ == '__main__':
 
